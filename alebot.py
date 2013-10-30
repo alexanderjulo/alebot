5f64ac3aa0c90cf871a029e9a752fe5fbeb504c4
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2013-10-30 03:18:12 +0100

    Initial version of the bot.

---
+++ alebot.py
@@ -0,0 +1,390 @@
+from asynchat import async_chat
+import asyncore
+import socket
+import fnmatch
+
+
+class Bot(async_chat):
+
+    """
+        The main bot class, where all the magic happens.
+
+        This class handles the socket and all incoming and outgoing
+        data. This classes methods should be used for sending data.
+
+        It supplies a hook function with which plugins can register
+        their hooks to be notified if fitting data can be received.
+
+        Please check out :class:`Hook` and :func:`hook` to find out
+        how hooking your plugins in works.
+
+        Please note that this bot does absolutely nothing by itself.
+
+        It won't even answer pings or identify. But there are some
+        system plugins to do that. Check the plugins folder.
+    """
+
+    Hooks = []
+
+    def __init__(self):
+        """
+            Initiates the parent, some necessary variables, and
+            instantiates the hooks.
+        """
+        async_chat.__init__(self)
+        self.set_terminator('\r\n')
+        self.buffer = ''
+        self.hooks = []
+        for Hook in self.Hooks:
+            self.hooks.append(Hook(self))
+
+    @classmethod
+    def hook(cls, Hook):
+        """
+            This method will register a hook with the bot. It is
+            supposed to be used as a decorator, but can also be used
+            as a normal function. Please see the :class:`Hook` class
+            documentation for an overview what a hook should look like.
+
+            This method will save the :class:`Hook` class with the
+            :class:`Bot` class until the :func:`__init__` function
+            instantiates the hooks.
+        """
+        cls.Hooks.append(Hook)
+
+    def find_hooks(self):
+        """
+            Will load all the hooks from the plugin folder. It will
+            only load them though! The plugins still have to register
+            themselves with the :func:`hook` function.
+
+            This function does not do anything yet. Plugins have to be
+            in the the same file as the bot itself.
+        """
+        pass
+
+    def call_hooks(self, event):
+        for hook in self.hooks:
+            if hook.match(event):
+                hook.call(event)
+
+    def connect(self):
+        """
+            Creates a socket and kicks off the connection process.
+        """
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        async_chat.connect(self, ('irc.freenode.net', 6667))
+        asyncore.loop()
+
+    def handle_connect(self):
+        """
+            As soon as the socket is connected, the (made up) event
+            `SOCK_CONNECTED` will be called! Use it to identify
+            yourself. This bot does NOTHING by itself.
+        """
+        event = Event('SOCK_CONNECTED')
+        self.call_hooks(event)
+
+    def send_raw(self, data):
+        """
+            Sends raw commands to the server. Only adds CLRF as a suffix.
+
+                :param data: the IRC command and body to send, fully
+                formatted as such.
+        """
+        self.push('%s\r\n' % data)
+
+    def collect_incoming_data(self, data):
+        """
+            Collects the incoming data and adds it to the buffer.
+
+                :param data: The received data from the server.
+
+            **Please do not use this function manually! It is only
+            to be used by asnchat!**
+        """
+        self.buffer += data
+
+    def found_terminator(self):
+        """
+            As IRC is a line based protocol which means every command
+            is in its own line, this function is called as soon as a
+            line and thus a command has been completely received.
+
+            The line is read from the buffer and the buffer cleared.
+
+            The function only does very basic syntax correction, to
+            clear up input that does not match the usual format.
+
+            I.e. a PING command does not follow the usual syntax.
+
+            Thusly the function will get the relevant parameter and
+            fill it into the usual structure.
+
+            This function will call the func:`call_hooks` function with
+            the extracted data.
+        """
+
+        line = self.buffer
+        self.buffer = ''
+
+        print line
+        line = line.split(' ', 3)
+
+        event = Event()
+
+        if (line[0][0] == ':'):
+
+            event.user = line[0][1:]
+            event.name = line[1]
+            event.target = line[2]
+
+            if (line[1] != 'JOIN'):
+                event.body = line[3][1:]
+
+        elif (line[0] == 'PING'):
+            event.name = line[0]
+            event.body = line[1][1:]
+
+        self.call_hooks(event)
+
+
+class Event(object):
+
+    """
+        The :class:`Event` class is used to store data about the
+        event and provide some helper methods that should save you
+        some string manipulation.
+
+        .. attribute:: name
+
+            The name attribute should always be given and will be a
+            `string`, either as defined in the IRC RFC (event number
+            or command) or one of the following custom events:
+
+                `SOCK_CONNECTED`: Sent as soon as the socket is
+                connected.
+
+        Depending on the type of the event there might be one or me of
+        the following attributes not empty (not `None`):
+
+        .. attribute:: user
+
+            The user that caused the event. In case it is a channel or
+            private message, it is the sender of the message, in case
+            of a join, the joining person, in case of a kick, the
+            kicking person and so on. If this is an actual user, and
+            not a server, :attr:`nick`, :attr:`ident` and :attr`host`
+            should be available, too.
+
+            If this is a user, it will be in the format of:
+
+                `nick!ident@host`
+
+            If it is a server, it will be in the form of:
+
+                `subdomain.domain.zone`
+
+        .. attribute:: body
+
+            If the event has a message, reason or a similar thing,
+            you will find it in body.
+
+        .. attibute:: target
+
+            The target of the action, a channel if it is a channel
+            message, the bot's nick if it is a private one or anything
+            else.
+    """
+
+    def __init__(self, name=None, user=None, target=None, body=None):
+        self.name = name
+        self.user = user
+        self.body = body
+        self.target = target
+        self._nick = False
+        self._ident = False
+        self._host = False
+
+    def _splitnickidenthost(self):
+        split = self.user.split('!')
+        if len(split) == 1:
+            return (None, None, None)
+        nick = split[0]
+        split = split[1].split('@')
+        return (nick, split[0], split[1])
+
+    @property
+    def nick(self):
+        if self._nick == False:
+            self._nick = self._splitnickidenthost()[0]
+        return self._nick
+
+    @property
+    def ident(self):
+        if self._ident == False:
+            self._ident = self._splitnickidenthost()[1]
+        return self._ident
+
+    @property
+    def host(self):
+        if self._host == False:
+            self._host = self._splitnickidenthost[2]
+        return self._host
+
+
+class Hook(object):
+
+    """
+        This is just a possible implementation for a hook. Every hook
+        that registers itself with the bot has to implement two
+        functions:
+
+
+            :func:`__init__`
+            :func:`match`
+            :func:`call`
+
+        Read these functions documentation to see what they should do.
+
+        Every hook that actually is supposed to be used, has to be
+        prefixed by the @Bot.hook decorator. Doing so automatically
+        adds the hook to the bot. The hook will be instantiated **once**
+        on startup and then kept in memory until the bot dies.
+
+        This means you can do some magic processing like reading config
+        files and stuff in the beginning. As soon as your bot will be
+        initialized there will be a bot instance present although
+        you won't be able to send data in the beginning.
+    """
+
+    def __init__(self, bot):
+        """
+            The :func:`__init__` function does not have to be overriden.
+            It is expected to accept one parameter:
+
+                :param bot: the bot instance
+
+            It should save the bot instance to itself (as this
+            implementation does). The bot instance can be used to
+            easily send data, change the nick or similar stuff.
+        """
+        self.bot = bot
+
+    def send_raw(self, data):
+        """
+            This function is just a shortcut to func:`Bot.send_raw`.
+        """
+        self.bot.send_raw(data)
+
+    def match(self, event):
+        """
+            This function is used to evaluate whether the hook wants
+            to react to the event that is passed on. It has to accept
+            one parameter:
+
+                :param event: an instance of the :class:`Event` class
+
+            Although theoretically possible I recommend to separate
+            the evaluation of a match and the actual reaction for the
+            code's clearnesses sake.
+
+            This way it is very easy to determine what triggers this
+            hook.
+
+                :returns: Either `True` or `False`, depending on
+                whether a match is given or not.
+
+            As this is a callback you have very high flexibility
+            regarding your matching. You can match combinations of
+            nick, ident, host, events, target and body.
+
+            Check out the :class:`Event` class to see what event data
+            is available.
+
+            You can also use a regex or vary matches based on the time
+            or the weather. Whatever you want.
+        """
+        raise NotImplementedError()
+
+    def call(self, event):
+        """
+            In case that your :func:`match` function returned `True`
+            this function will be called. It will again receive:
+
+                :param event: an instance of the :class:`Event` class
+
+            Now you are free to send data or do whatever you have to
+            do.
+        """
+        raise NotImplementedError()
+
+
+class ConnectionReadyHook(Hook):
+
+    """
+        This is a hook that can be subclassed in case you want to react
+        on a irc connection that is ready for commands. It waits for
+        the end of the motd, or the message that there is no motd.
+
+        The :func:`match` function was implemented to listen to the
+        correct events. You will just have to overwrite the :func`call`
+        to actually do something.
+    """
+
+    def match(self, event):
+        return (event.name == '376' or event.name == '422')
+
+
+@Bot.hook
+class SocketConnectedHook(Hook):
+
+    """
+        As the bot does nothing itself, this plugin takes care of
+        identifying the bot with the server. Yeah, seriously.
+
+        It uses the made up `SOCK_CONNECTED` event that is not even
+        an actual IRC event..
+    """
+
+    def match(self, event):
+        return (event.name == 'SOCK_CONNECTED')
+
+    def call(self, event):
+        self.send_raw("NICK %s" % 'alebot')
+        self.send_raw("USER %s * %s :%s" % ('alebot', 'alebot', 'alebot'))
+
+
+@Bot.hook
+class PingPong(Hook):
+
+    """
+        As the bot does nothing by itself, this plugin takes care of
+        sending PONGs as answer to pings, as the bot won't even do that.
+
+        It matches the `PING` event to do that.
+    """
+
+    def match(self, event):
+        return (event.name == 'PING')
+
+    def call(self, event):
+        print('Received ping, sending pong.')
+        self.send_raw('PONG %s' % event.body)
+
+
+@Bot.hook
+class JoinOnConnect(ConnectionReadyHook):
+
+    """
+        This hook will join a specific channel upon connection.
+    """
+
+    def call(self, event):
+        print('Connection successful. Joining a channel now.')
+        self.send_raw('JOIN %s' % '#aletest')
+
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.connect()
