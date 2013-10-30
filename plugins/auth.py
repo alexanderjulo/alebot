bf8840ca651c61ddcbf98e9a237129e28d10335f
Alexander Jung-Loddenkemper <alexander.julo@googlemail.com>
2013-10-30 18:21:15 +0100

    provide helper classes.

--- plugins/auth.py
+++ plugins/auth.py
@@ -1,6 +1,8 @@
 from alebot import Alebot, Hook
 from functools import wraps
 
+default = Alebot.get_plugin('default')
+
 def admin_required(f):
     @wraps(f)
     def auth_and_match(self, event):
@@ -9,3 +11,16 @@ def admin_required(f):
             return False
         return f(self, event)
     return auth_and_match
+
+
+class AdminCommandHook(default.CommandHook):
+
+    """
+        Just a shortcut for simple commands that require admin
+        permissions.
+    """
+
+    @admin_required
+    def match(self, event):
+        print(self)
+        return default.CommandHook.match(self, event)
