efc0af84137867f36c0c1d9b3f9734bcfe59db4e
Markus Cisler <m@kuchen.io>
2014-06-27 11:14:07 +0200

    Update cli.py

--- alebot/cli.py
+++ alebot/cli.py
@@ -5,6 +5,6 @@ from . import Alebot
 @click.command()
 @click.option("--path", default=".",
               help="Path in which confi.json and plugins are.")
-def run(path, daemonize):
+def run(path):
     alebot = Alebot(path)
     alebot.connect()
